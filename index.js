f3a8e85279628cc3e184ad8981f9446a34345ebb
ashiqurras <ashiqurrussel2@gmail.com>
2022-10-25 18:09:14 +0200

    course api

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const categories = require("./data/categories");
+const course = require("./data/course.json");
+
+app.get("/", (req, res) => {
+  res.send("Course API Running");
+});
+
+app.get("/course-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/course", (req, res) => {
+  res.send(course);
+});
 
 app.listen(port, () => {
   console.log("Dragon News Server running on port", port);
